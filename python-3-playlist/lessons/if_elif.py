013cc075ace56799e4e4e633654893cd057351d6
Ryan Webb <dzanfox@gmail.com>
2017-12-05 21:20:20 -0600

    quick if statement lesson

---
+++ python-3-playlist/lessons/if_elif.py
@@ -0,0 +1,23 @@
+#if statements
+
+#operators
+# <, >, ==, !=, >=, <=
+
+# age = int(input("Enter your age: "))
+
+# if age < 10:
+#     #code block
+#     print('You are young, strange one')
+# elif age < 40:
+#     #you can have as many elifs as you want, elif = else if
+#     print('The fire in you is strong, strange one')
+# else:
+#     #default if other criteria don't match
+#     print("you are wise beyond doubt, strange one")
+
+meaty = input("do you eat meat? (y/n): ")
+
+if meaty == 'y':
+    print("Here is the meaty menu...")
+else:
+    print("Here is the veggie menu...")
