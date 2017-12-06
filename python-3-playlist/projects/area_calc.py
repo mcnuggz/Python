f9eeb445e9e9f98e8d34affda0306095627de091
Ryan Webb <dzanfox@gmail.com>
2017-12-05 20:28:04 -0600

    input lesson finished

---
+++ python-3-playlist/projects/area_calc.py
@@ -0,0 +1,13 @@
+# comment example
+
+# name = input("Tell me your name, dude: ")
+# age = input("And how old are you, " + name + "? ")
+# print("Sup, " + name)
+# print("You are " + age + " years old")
+# print(name, 'you are', age)
+
+#calcuate the area of a circle
+#Formula: pi r squared
+radius = int(input("Enter a radius of your circle (m): "))
+area = 3.142 * radius**2
+print('The area of your circle is', area)
