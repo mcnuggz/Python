9ffda902be41a06152ac83dcb5b84950059ad4a2
Ryan Webb <dzanfox@gmail.com>
2017-12-06 19:52:42 -0600

    dictionary lesson added

---
+++ python-3-playlist/lessons/dictionaries.py
@@ -0,0 +1,20 @@
+#prints to terminal
+
+def ninja_intro(dictionary):
+    for key, val in dictionary.items():
+        print(f'I am {key}, and I am a {val} belt')
+
+ninja_belts = {}
+
+while True:
+    ninja_name = input('Enter a ninja name: ')
+    ninja_belt = input('Enter a belt color: ')
+    ninja_belts[ninja_name] = ninja_belt
+
+    another = input('Add another? (y/n): ')
+    if another == 'y' or another == 'yes':
+        continue
+    else:
+        break;
+
+ninja_intro(ninja_belts)
